b8152a72241782b4c085fdacf0984dba8b0b8d02
The-OP <The-OP@users.noreply.github.com>
2015-06-16 16:07:53 +0300

    Update prefs_3.js

--- prefs_3.js
+++ prefs_3.js
@@ -52,16 +52,6 @@ user_pref("media.mediasource.enabled", false);
 user_pref("media.mediasource.mp4.enabled", false);
 user_pref("media.mediasource.webm.enabled", false);
 
-// Отключает поддержку соответствующих форматов/кодеков.
-user_pref("media.fragmented-mp4.enabled", false);
-user_pref("media.fragmented-mp4.exposed", false);
-user_pref("media.fragmented-mp4.ffmpeg.enabled", false);
-user_pref("media.ogg.enabled", false);
-user_pref("media.opus.enabled", false);
-user_pref("media.webm.enabled", false);
-user_pref("media.raw.enabled", false);
-user_pref("media.wave.enabled", false);
-
 // Отключает декодирование через GStreamer.
 user_pref("media.gstreamer.enabled", false);
 
@@ -115,7 +105,7 @@ user_pref("general.appname.override", "Netscape");
 user_pref("general.appversion.override", "5.0 (Windows)");
 user_pref("general.oscpu.override", "Windows NT 6.1");
 user_pref("general.platform.override", "Win32");
-user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0");
+user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0");
 user_pref("general.productSub.override", "20100101");
 user_pref("general.buildID.override", "20100101");
 user_pref("browser.startup.homepage_override.buildID", "20100101");
@@ -136,15 +126,19 @@ user_pref("media.webaudio.enabled", false );
 // Третий уровень - отключение JIT-компилятора, SVG и обработку JavaScript только для HTTPS.
 user_pref("javascript.options.baselinejit.content", false);
 user_pref("gfx.font_rendering.graphite.enabled", false);
-// Наивысший уровень - отключение внешних шрифтов, JavaScript и всех кодеков за исключением
-// WebM, который останется доступным только после нажатия.
+// Наивысший уровень - отключение внешних шрифтов, JavaScript и всех кодеков.
 // Отключение внешних шрифтов. На выбор одна из настроек:
 user_pref("gfx.downloadable_fonts.enabled", false);
 user_pref("noscript.forbidFonts", true);
 // Полное отключение JavaScript
 user_pref("noscript.global", false);
-// Отключение поддержки кодеков, кроме WebM (он будет воспроизводиться при нажатии).
+// Отключает поддержку соответствующих форматов/кодеков.
+user_pref("media.fragmented-mp4.enabled", false);
+user_pref("media.fragmented-mp4.exposed", false);
+user_pref("media.fragmented-mp4.ffmpeg.enabled", false);
 user_pref("media.ogg.enabled", false);
 user_pref("media.opus.enabled", false);
+user_pref("media.webm.enabled", false);
+user_pref("media.raw.enabled", false);
 user_pref("media.wave.enabled", false);
 user_pref("media.apple.mp3.enabled", false);
